e8c364b30f2f49455df57f3705a0ed57f7c34977
Christos Gkantidis <christosg8@gmail.com>
2022-06-09 12:28:14 +0200

    Add a read/write lock example with Boost shared_mutex, in case of <C++17 standard

--- 02_sharing_data/06_read_write_lock_boost.cpp
+++ 02_sharing_data/06_read_write_lock_boost.cpp
@@ -1,35 +1,36 @@
 #include <algorithm>
 #include <iostream>
 #include <map>
-#include <mutex>
 #include <shared_mutex>
 #include <string>
 #include <thread>
 #include <vector>
+#include <boost/thread/locks.hpp>
+#include <boost/thread/shared_mutex.hpp>
 
 class dns_entry {};
 
 class dns_cache {
   std::map<std::string, dns_entry> _entries;
-  mutable std::shared_mutex _entry_mutex;
+  mutable boost::shared_mutex _entry_mutex;
 
 public:
   dns_entry find_entry(std::string const &domain) const {
     // multiple readers
-    std::shared_lock<std::shared_mutex> lk(_entry_mutex);
+    boost::shared_lock<boost::shared_mutex> lk(_entry_mutex);
     auto find_it = _entries.find(domain);
     return find_it == _entries.end() ? dns_entry() : find_it->second;
   }
 
   void update_or_add_entry(std::string const &domain, dns_entry const &dns_details) {
     // single writer
-    std::lock_guard<std::shared_mutex> lk(_entry_mutex);
+    boost::lock_guard<boost::shared_mutex> lk(_entry_mutex);
     _entries[domain] = dns_details;
   }
 
   void print_domains() const {
     // multiple readers
-    std::shared_lock<std::shared_mutex> lk(_entry_mutex);
+    boost::shared_lock<boost::shared_mutex> lk(_entry_mutex);
     for (auto const &p : _entries) {
       std::cout << p.first << "\n";
     }
